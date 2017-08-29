a967cddf82da0ba56938d95520d78370f4ad8f94
Dragon_Iron <maxl13@ukr.net>
2017-08-29 23:18:47 +0300

    Змейка задвигалась

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Snake
@@ -13,6 +14,7 @@ namespace Snake
             Console.SetWindowSize(80, 25);
             Console.SetBufferSize(80, 25);
 
+            // Отрисовка рамки
             HorizontalLine topLine = new HorizontalLine(0, 78, 0, '+');
             HorizontalLine bottomLine = new HorizontalLine(0, 78, 24, '+');
             VerticalLine rightLine = new VerticalLine(0, 24, 0, '+');
@@ -23,11 +25,26 @@ namespace Snake
             rightLine.DrawFigure();
             leftLine.DrawFigure();
 
+            // Отрисовка змейки
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.DrawFigure();
-
-            Console.Read();
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
 
         }
     }
