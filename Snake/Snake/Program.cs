faeb927b1ccddb850617524230118ce46fdf6c89
Dragon_Iron <maxl13@ukr.net>
2017-08-30 18:09:44 +0300

    Создание еды

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -30,9 +30,19 @@ namespace Snake
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.DrawFigure();
 
+            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
+
             // Управление змейкой
             while (true)
             {
+                if (snake.Eat(food))
+                {
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else { snake.Move(); }
                 if (Console.KeyAvailable)
                 {
                     ConsoleKeyInfo key = Console.ReadKey();
